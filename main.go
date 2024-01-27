44a5ee3ea170293f3426c3abae87887a78cb4271
Craig Skinfill <craig.skinfill@gmail.com>
2024-01-27 18:16:22 -0500

    working on getting otel working

--- main.go
+++ main.go
@@ -1,96 +1,190 @@
 package main
 
 import (
-    "context"
-    "encoding/json"
-    "fmt"
-    "net/http"
+	"context"
+	"database/sql"
+	"encoding/json"
+	"log"
 
-    "github.com/gorilla/mux"
-    "go.opentelemetry.io/otel"
-    "go.opentelemetry.io/otel/exporters/jaeger"
-    "go.opentelemetry.io/otel/exporters/prometheus"
-    "go.opentelemetry.io/otel/sdk/resource"
-    sdktrace "go.opentelemetry.io/otel/sdk/trace"
-    "go.opentelemetry.io/otel/semconv"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/uptrace/opentelemetry-go-extra/otelsql"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux"
+	"go.opentelemetry.io/otel"
 )
 
 type Service struct {
-    ID    uint    `json:"id"`
-    Name  string `json:"name"`
-    Description string `json:"description"`
-    Versions uint `json:"versions"`
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Versions    uint   `json:"versions"`
 }
 
-var service []Service
-
-// Initialize OpenTelemetry
-func initTracer() func() {
-    exporter, err := jaeger.New(jaeger.WithCollectorEndpoint()) // Replace with your collector endpoint
-    if err != nil {
-        panic(err)
-    }
-
-    tp := sdktrace.NewTracerProvider(
-        sdktrace.WithSampler(sdktrace.AlwaysSample()),
-        sdktrace.WithResource(resource.NewWithAttributes(
-            semconv.ServiceNameKey.String("services"),
-        )),
-        sdktrace.WithBatcher(exporter),
-    )
-
-    otel.SetTracerProvider(tp)
-
-    exporter, err = prometheus.New(prometheus.Config{})
-    if err != nil {
-        panic(err)
-    }
-
-    otel.SetMeterProvider(exporter.MeterProvider())
-
-    return func() {
-        _ = exporter.Shutdown(context.Background())
-        _ = tp.Shutdown(context.Background())
-    }
+type ServiceRepository interface {
+	Services(ctx context.Context) ([]Service, error)
+	Service(ctx context.Context, id int) (*Service, error)
 }
 
-func getServices(w http.ResponseWriter, r *http.Request) {
-    ctx, span := otel.GetTracerProvider().Tracer("services").Start(r.Context(), "geServices")
-    defer span.End()
-
-    span.AddEvent("Retrieving services from store")
-    json.NewEncoder(w).Encode(services)
+type ServiceHandler struct {
+	repository ServiceRepository
 }
 
-func getService(w http.ResponseWriter, r *http.Request) {
-    ctx, span := otel.GetTracerProvider().Tracer("service").Start(r.Context(), "getService")
-    defer span.End()
-
-    vars := mux.Vars(r)
-    id, err := strconv.Atoi(vars["id"])
-    if err != nil {
-        http.Error(w, "Invalid service ID", http.StatusBadRequest)
-        return
-    }
-
-    for _, service := range service {
-        if service.ID == id {
-            json.NewEncoder(w).Encode(service)
-            return
-        }
-    }
-
-    http.Error(w, "Service not found", http.StatusNotFound)
+func NewServiceHandler(serviceRepo ServiceRepository) ServiceHandler {
+	return ServiceHandler{repository: serviceRepo}
 }
+
+func (h *ServiceHandler) GetServices(w http.ResponseWriter, r *http.Request) {
+	services, err := h.repository.Services(r.Context())
+	if err != nil {
+		log.Println("Something bad happened", err)
+		http.Error(w, "Badness", http.StatusInternalServerError)
+	}
+	json.NewEncoder(w).Encode(services)
+}
+func (h *ServiceHandler) GetService(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, "Invalid service ID", http.StatusBadRequest)
+		return
+	}
+
+	service, err := h.repository.Service(r.Context(), id)
+
+	if err != nil {
+		log.Println("Something bad happened", err)
+		http.Error(w, "Badness", http.StatusInternalServerError)
+	}
+
+	if service != nil {
+		json.NewEncoder(w).Encode(service)
+		return
+	}
+
+	http.Error(w, "Service not found", http.StatusNotFound)
+}
+
+var tracer = otel.Tracer("")
 
 func main() {
-    defer initTracer()() // Initialize and shutdown OpenTelemetry
 
-    router := mux.NewRouter()
-    router.HandleFunc("/services", getServices).Methods("GET")
-    router.HandleFunc("/service/{id}", getService).Methods("GET")
-    router.Handle("/metrics", otel.Handler()) // Handle Prometheus metrics
+	otelShutdown, err := setupOTelSDK(context.Background())
 
-    fmt.Println("Starting service on port 8080")
-    http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Panic("Badness", err)
+	}
+	defer func() {
+		otelShutdown(context.Background())
+	}()
+
+	repo, err := NewDbRepo("services.db")
+	if err != nil {
+		log.Panic(err)
+	}
+	handler := NewServiceHandler(repo)
+
+	router := mux.NewRouter()
+	router.Use(otelmux.Middleware("services"))
+	router.HandleFunc("/services", handler.GetServices).Methods("GET")
+	router.HandleFunc("/service/{id}", handler.GetService).Methods("GET")
+	router.Handle("/metrics", promhttp.Handler())
+
+	fmt.Println("Starting service on port 8080")
+	http.ListenAndServe(":8080", router)
+}
+
+type InMemoryRepo struct {
+	_services []Service
+}
+
+func NewInMemoryRepo() (ServiceRepository, error) {
+	services := []Service{}
+
+	service := Service{ID: 1, Name: "Locate Us", Description: "Awesomeness is HERE!", Versions: 3}
+	services = append(services, service)
+
+	service = Service{ID: 2, Name: "Contact Us", Description: "How can I find you?!", Versions: 2}
+	services = append(services, service)
+
+	return &InMemoryRepo{_services: services}, nil
+}
+
+func (r *InMemoryRepo) Services(ctx context.Context) ([]Service, error) {
+	_, span := tracer.Start(ctx, "Services")
+	defer span.End()
+	return r._services, nil
+}
+
+func (r *InMemoryRepo) Service(ctx context.Context, id int) (*Service, error) {
+	_, span := tracer.Start(ctx, "Service")
+	defer span.End()
+
+	services, err := r.Services(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, service := range services {
+		if service.ID == id {
+			return &service, nil
+		}
+	}
+
+	return nil, nil
+}
+
+type DbRepo struct {
+	db *sql.DB
+}
+
+func NewDbRepo(database string) (*DbRepo, error) {
+	db, err := otelsql.Open("sqlite3", database)
+	if err != nil {
+		return nil, err
+	}
+	return &DbRepo{
+		db: db,
+	}, nil
+}
+
+func (r *DbRepo) Services(ctx context.Context) ([]Service, error) {
+	_, span := tracer.Start(ctx, "Services")
+	defer span.End()
+	rows, err := r.db.QueryContext(ctx, "SELECT * FROM services")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	data := []Service{}
+	for rows.Next() {
+		service := Service{}
+		err = rows.Scan(&service.ID, &service.Name, &service.Description, &service.Versions)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, service)
+	}
+	return data, nil
+}
+
+func (r *DbRepo) Service(ctx context.Context, id int) (*Service, error) {
+	_, span := tracer.Start(ctx, "Service")
+	defer span.End()
+	row := r.db.QueryRowContext(ctx, "SELECT * FROM services WHERE id=?", id)
+
+	// Parse row into Activity struct
+	service := Service{}
+	var err error
+	if err = row.Scan(&service.ID, &service.Name, &service.Description, &service.Versions); err == sql.ErrNoRows {
+		log.Printf("Id not found")
+		return &Service{}, err
+	}
+	return &service, nil
 }
---
+++ otel.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"context"
+	"errors"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/prometheus"
+	stdout "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+)
+
+// setupOTelSDK bootstraps the OpenTelemetry pipeline.
+// If it does not return an error, make sure to call shutdown for proper cleanup.
+func setupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
+
+	// shutdown calls cleanup functions registered via shutdownFuncs.
+	// The errors from the calls are joined.
+	// Each registered cleanup will be invoked once.
+	shutdown = func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+		shutdownFuncs = nil
+		return err
+	}
+
+	// handleErr calls shutdown for cleanup and makes sure that all errors are returned.
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	// Set up propagator.
+	prop := newPropagator()
+	otel.SetTextMapPropagator(prop)
+
+	// Set up trace provider.
+	tracerProvider, err := newTraceProvider()
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
+	otel.SetTracerProvider(tracerProvider)
+
+	// Set up meter provider.
+	meterProvider, err := newMeterProvider()
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
+	otel.SetMeterProvider(meterProvider)
+
+	return
+}
+
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+}
+
+func newTraceProvider() (*trace.TracerProvider, error) {
+	// exporter, err := otlptracehttp.New(context.Background())
+	exporter, err := stdout.New(stdout.WithPrettyPrint())
+	if err != nil {
+		panic(err)
+	}
+
+	tp := trace.NewTracerProvider(
+		trace.WithSampler(trace.AlwaysSample()),
+		trace.WithResource(resource.NewWithAttributes(semconv.SchemaURL,
+			semconv.ServiceNameKey.String("services"),
+		)),
+		trace.WithBatcher(exporter),
+	)
+	return tp, nil
+}
+
+func newMeterProvider() (*metric.MeterProvider, error) {
+	metricExporter, err := prometheus.New()
+	if err != nil {
+		return nil, err
+	}
+
+	meterProvider := metric.NewMeterProvider(metric.WithReader(metricExporter))
+	return meterProvider, nil
+}
